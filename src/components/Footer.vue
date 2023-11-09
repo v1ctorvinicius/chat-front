418076d4485305a49a4ce3b780c89d7950a890d5
Victor Santos <victor.santos@tcema.tc.br>
2023-11-09 11:38:07 -0300

    ok

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,12 @@
 <template>
-	<footer class="blue">
-		Victor <br> 2023
-	</footer>
+  <footer class="blue">
+    Victor <br />
+    2023
+  </footer>
 </template>
 <style scoped>
 footer {
-	text-align: center;
-	padding: 10px;
+  text-align: center;
+  padding: 10px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,36 @@
 <script setup lang="ts">
-import Footer from '../components/Footer.vue';
+import { ref } from "vue";
+import Footer from "../components/Footer.vue";
+let chats = ref([]);
 </script>
 
 <template>
-	<div class="vintage text-white ">
-		<header>
-			<h1 class="blue-whale-alpha padding-3 margin-5">Web Chat Home</h1>
-		</header>
-		<main id="rooms" class="blue-whale-alpha padding-3 margin-5">
-			<h2>Salas:</h2>
-			<ul>
-				<li>sala</li>
-				<li>sala</li>
-				<li>sala</li>
-				<li>sala</li>
-				<li>sala</li>
-				<li>sala</li>
-			</ul>
-		</main>
-		<section id="create" class="blue-whale-alpha margin-5">
-			<table>
-				<tr>
-					<td>
-						<a href="">criar</a>
-					</td>
-					<td>
-						<a href="">buscar</a>
-					</td>
-				</tr>
-			</table>
-		</section>
-		<Footer />
-	</div>
+  <div class="vintage text-white">
+    <header>
+      <h1 class="blue-whale-alpha padding-3 margin-5">Web Chat Home</h1>
+    </header>
+    <main id="rooms" class="blue-whale-alpha padding-3 margin-5">
+      <h2>Salas:</h2>
+      <div></div>
+    </main>
+    <section id="create" class="blue-whale-alpha margin-5">
+      <table>
+        <tr>
+          <td>
+            <a href="">criar</a>
+          </td>
+          <td>
+            <a href="">buscar</a>
+          </td>
+        </tr>
+      </table>
+    </section>
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
-	a {
-		color: white;
-	}
+a {
+  color: white;
+}
 </style>
