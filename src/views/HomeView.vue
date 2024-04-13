6a7f310a5b4dd53039e4bb928ddfe497eab2ca2d
victor santos <victor.santos@tce.ma.gov.br>
2024-04-13 20:18:09 -0300

    feat(HomeView.vue): add primevue Dialog

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,33 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import ChatRoomCard from "@/components/ChatRoomCard.vue";
-import { NButton, NInput } from "naive-ui";
+
 import axios from "axios";
-import type ChatRoom from "../types/ChatRoom";
+import ChatCard from "@/components/ChatCard.vue";
+import type chat from "../types/chat";
 
-let chats = ref<ChatRoom[]>([]);
+let chats = ref<chat[]>([]);
 let chatCount = 0;
-let newRoomName = "";
+let newChatName = "";
+const visible = ref(false);
 
-axios.get("http://localhost:8081/chats/").then((res) => (chats.value = res.data));
+axios.get("http://localhost:8080/chats/").then((res) => (chats.value = res.data));
 
 const createChat = () => {
-  axios.post("http://localhost:8081/chats/", { name: newRoomName }).then((res) => {
+  axios.post("http://localhost:8080/chats/", { name: newChatName }).then((res) => {
     chats.value.push(res.data);
-  });
-  console.log("chats: ", chats.value[0]);
+  }).catch((err) => console.error(err));
 };
+
+const changeVisibleHandler = () => {
+  visible.value = !visible.value
+  console.log(visible.value);
+
+}
 </script>
 
 <template>
-  <div class="text-white">
-    
+  <div class="text-white vintage">
+
     <header>
       <h1 class="blue-whale-alpha margin-5 padding-3 border-radius-10">
         Web Chat Home
@@ -30,15 +36,15 @@ const createChat = () => {
     <main id="rooms" class="blue-whale-alpha padding-3 margin-5 border-radius-10">
       <h2>Chats:</h2>
       <section class="chats-container">
-        <ChatRoomCard v-for="chat in chats" :chatName="chat.name" />
+        <ChatCard v-for="chat in chats" :chatName="chat.name" @change-visible="changeVisibleHandler" />
       </section>
     </main>
     <section id="create" class="blue-whale-alpha margin-5">
       <table>
         <tr>
           <td>
-            <input type="text" v-model="newRoomName">
-            <n-button @click="createChat" type="info"> Criar chat </n-button>
+            <input type="text" v-model="newChatName">
+            <Button @click="createChat" label="create chat" icon="pi pi-plus" severity="success" />
           </td>
           <td>
             <a href="">buscar</a>
@@ -47,7 +53,23 @@ const createChat = () => {
       </table>
     </section>
   </div>
+
+  <Dialog :visible="visible" modal :closable="true" header="chat modal" :style="{ width: '25rem' }">
+    <div class="p-fluid">
+      <div class="p-field">
+        <label for="messageInput">Digite sua mensagem:</label>
+      </div>
+    </div>
+    <template #footer>
+      <div class="p-d-flex p-jc-end">
+        <Button label="Enviar" icon="pi pi-send" />
+        <Button label="Fechar" icon="pi pi-times" class="p-button-secondary" />
+      </div>
+    </template>
+
+  </Dialog>
 </template>
+
 
 <style scoped>
 a {
@@ -76,3 +98,4 @@ a {
   border: 1px solid red;
 }
 </style>
+../types/chat
