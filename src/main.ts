0f951afd15f866c8477f6fca86148ca6cf0d822b
victor santos <victor.santos@tce.ma.gov.br>
2024-04-13 20:11:09 -0300

    feat(main.ts): add primevue button

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,13 @@ import PrimeVue from "primevue/config";
 import "primevue/resources/themes/aura-light-green/theme.css";
 import Dialog from "primevue/dialog";
 import Card from "primevue/card";
+import Button from 'primevue/button';
+
 
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue);
 app.component("Dialog", Dialog);
 app.component("Card", Card);
+app.component("Button", Button);
 app.mount("#app");
