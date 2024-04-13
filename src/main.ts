0e8bd22d8c9d40d1fd4721303e092d373fca9121
victor santos <victor.santos@tce.ma.gov.br>
2024-04-13 19:40:07 -0300

    refact

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import "./assets/style.css";
-import 'primevue/resources/themes/aura-light-green/theme.css'
 
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
 import PrimeVue from "primevue/config";
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import Dialog from 'primevue/dialog';
 
 const app = createApp(App);
 
-app.use(router);
 app.use(PrimeVue);
+app.use(router);
+
+app.component('Dialog', Dialog);
 
 app.mount("#app");
