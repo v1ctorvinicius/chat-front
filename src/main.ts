fd64a07d362fdd1cbcbbed206d08993c0ee3a9f2
victor santos <victor.santos@tce.ma.gov.br>
2024-04-13 19:25:54 -0300

    feat/chore: add cors and primevue, remove naive-ui

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import "./assets/style.css";
+import 'primevue/resources/themes/aura-light-green/theme.css'
 
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from "primevue/config";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue);
 
 app.mount("#app");
