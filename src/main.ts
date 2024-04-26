3d56bdbb1b62a1e802410b3b7e24ed3a6bcfc1fa
victor.santos <victor.santos@gmail.com>
2024-04-26 11:46:31 -0300

    feat(main.ts, package.json): add primevue icons

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from "./router";
 
 import PrimeVue from "primevue/config";
 import "primevue/resources/themes/aura-dark-purple/theme.css";
+import "primeicons/primeicons.css";
 import Dialog from "primevue/dialog";
 import Card from "primevue/card";
 import Button from "primevue/button";
