f3c3648f92bc2526c0ef5b75a9fda18e1f32c657
victor.santos <victor.santos@gmail.com>
2024-04-26 10:30:59 -0300

    style(ChatCard): max-height

--- src/components/ChatCard.vue
+++ src/components/ChatCard.vue
@@ -12,7 +12,7 @@ function cardClickHandler() {
 </script>
 
 <template>
-  <Card style=" width: 20rem; overflow: hidden" @click="cardClickHandler" class="card-container">
+  <Card style=" width: 15vw; max-height: 35vh; overflow: hidden" @click="cardClickHandler" class="card-container">
     <template #header>
       <img alt="user header" src="https://primefaces.org/cdn/primevue/images/usercard.png" style="width: 100%; " />
     </template>
@@ -22,7 +22,21 @@ function cardClickHandler() {
     <template #content>
       <p class="m-0">
         created by: user_name <br>
-
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus facilis eveniet recusandae, consequatur
+        illum debitis voluptates explicabo animi atque aspernatur vitae officiis asperiores suscipit commodi amet
+        voluptate libero fugit quas?
+        Necessitatibus quo debitis, nobis tempore hic repellendus, harum numquam ea beatae distinctio assumenda, iure
+        voluptatibus adipisci ab deleniti molestiae! Vel laudantium molestiae inventore veritatis fugiat tenetur
+        similique mollitia pariatur optio.
+        Eaque nihil voluptatibus vitae accusantium recusandae, voluptatum, ducimus minus voluptas et placeat autem,
+        blanditiis cum assumenda? Blanditiis quam cupiditate harum nihil porro deleniti ducimus, facere voluptas
+        pariatur rerum laboriosam maiores?
+        Accusamus quis labore est nihil eligendi quod commodi. Impedit, fuga ab adipisci mollitia totam iure libero ea
+        labore? Incidunt corporis molestiae tempora veritatis perspiciatis consequuntur delectus obcaecati quisquam
+        mollitia odit?
+        Voluptatem dolorem debitis omnis quibusdam, incidunt quidem architecto ex error aliquid perferendis harum quos.
+        Perspiciatis, animi suscipit cupiditate dolores excepturi nisi! Explicabo temporibus vel quae eveniet eum soluta
+        officia. Laborum.
       </p>
     </template>
     <template #footer>
